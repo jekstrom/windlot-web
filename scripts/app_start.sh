d3dad655dbc8f82ce8ac760fcd18b46530461374
james <james@jamesekstrom.com>
2021-02-20 15:06:08 -0800

    Update build scripts.

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/webapps/windlot
-npm start
-pm2 start npm --name "windlot-web" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+# cd /home/webapps/windlot
+# npm start
+# pm2 start npm --name "windlot-web" -- start
+# pm2 startup
+# pm2 save
+# pm2 restart all
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir /home/webapps
-mkdir /home/webapps/windlot
-cd /home/webapps/windlot
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-apt-get install -y nodejs npm
+# mkdir /home/webapps
+# mkdir /home/webapps/windlot
+# cd /home/webapps/windlot
+# curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+# apt-get install -y nodejs npm
